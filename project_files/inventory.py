974245410987bf2129f4e09fd8813a36c0fa7614
Fairooz Farzana Tasnia <tasniaf@berea.edu>
2025-11-29 11:13:42 -0500

    Some progress with remaining classes, completed product class(read the product data from txt file,and update the stock accordingly) and inventory class(initializing the accumulator,shows the sell products revenue,show inventory for company).

---
+++ project_files/inventory.py
@@ -0,0 +1,74 @@
+######################################################################
+# Author: Rafael, Fairooz
+# Username: hermozafreitasr, tasniaf
+#
+# Purpose: Create a barcode class to have it associated with a unique product in an inventory store
+#
+######################################################################
+# Acknowledgements:
+#
+# None: Original work
+
+# licensed under a Creative Commons
+# Attribution-Noncommercial-Share Alike 3.0 United States License.
+####################################################################################
+
+
+from product import *
+
+
+class Inventory:
+    def __init__(self, products=None):
+        if products is None:
+            self.products = []
+        else:
+            self.products = products
+        self.total_earnings = 0
+
+    def sell_product(self, code, quantity=1):
+        for product in self.products:
+            if product.code == code:
+                if product.update_stock(quantity):
+                    self.total_earnings += product.selling_price * quantity
+                    print(quantity, product.product_name, "sold")
+                    return True
+                else:
+                    return False
+        print("Product", code, "not found!")
+        return False
+
+    def show_inventory(self):
+        print("Current Inventory:")
+        for p in self.products:
+            print(p.display_info())
+
+    def show_summary(self):
+        print("Sales Summary")
+        print("Total Earnings:", self.total_earnings)
+        print("Products Sold:")
+        for p in self.products:
+            sold_quantity = p.initial_stock - p.stock
+            print(p.product_name, ":", sold_quantity, "sold")
+
+def main():
+    filename = input("Enter product file: ").strip()
+    if not os.path.isfile(filename):
+        print(filename, "not found! Using default 'products.txt'")
+        filename = "products.txt"
+
+    products = read_products_file(filename)
+
+    store = Inventory(products)
+
+    store.show_inventory()
+
+    code = input("Enter barcode to sell: ")
+    quantity = int(input("Enter quantity to sell: "))
+    store.sell_product(code, quantity)
+
+    store.show_inventory()
+    store.show_summary()
+
+
+if __name__ == "__main__":
+    main()
---
+++ project_files/product.py
@@ -0,0 +1,84 @@
+######################################################################
+# Author:  FairoozRafael,
+# Username: hermozafreitasr, tasniaf
+#
+# Purpose: Create a product class to have it in an inventory store and inherit the UPC class which holds the unique barcode and details of the product
+#
+######################################################################
+# Acknowledgements:
+#
+# None: Original work
+
+# licensed under a Creative Commons
+# Attribution-Noncommercial-Share Alike 3.0 United States License.
+####################################################################################
+
+import os
+from upc import UPC
+
+
+class Product(UPC):
+    def __init__(self, code=None, name=None, cost=None, price=None, manu=None, stock=0):
+        super().__init__(code, name, cost, price, manu)  # inherit UPC attributes
+        self.stock = stock
+        self.initial_stock = stock
+
+    def update_stock(self, quantity):
+
+        if quantity <= self.stock:
+            self.stock -= quantity
+            return True
+        else:
+            print("Not available")
+            return False
+
+    def display_info(self):
+
+        return ("Name:", self.product_name,
+                "Price:", self.selling_price,
+                "Barcode:", self.code,
+                "Stock:", self.stock)
+
+def read_products_file(filename):
+        if not os.path.isfile(filename):
+            print(filename, "not found!")
+            return []
+
+        products = []
+        with open(filename, 'r') as file_content:
+            num_products = int(file_content.readline().strip())
+
+            for _ in range(num_products):
+                product_data = []
+
+                for i in range(6):
+                    content = file_content.readline().strip("\n")
+                    key, value = content.split(":", 1)
+
+                    if key in ["Cost", "Price"]:
+                        value = float(value)
+                    elif key == "Stock":
+                        value = int(value)
+
+                    product_data.append(value)
+
+                p = Product(code=product_data[5], name=product_data[0], cost=product_data[1],
+                            price=product_data[2], manu=product_data[3], stock=product_data[4])
+                products.append(p)
+
+        return products
+
+def main():
+        filename = input("Enter product file: ").strip()
+        if not os.path.isfile(filename):
+            print(filename, "not found! Using default 'products.txt'")
+            filename = "products.txt"
+
+        products = read_products_file(filename)
+
+        print("Products available:")
+        for p in products:
+            print(p.display_info())
+
+if __name__ == "__main__":
+    main()
